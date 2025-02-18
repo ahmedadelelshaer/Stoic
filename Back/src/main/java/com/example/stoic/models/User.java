b7c54440caa38599bc747850d67ee0675597935b
fady <108768356+fadymofid@users.noreply.github.com>
2025-02-18 21:39:36 +0200

    user updated

--- Back/src/main/java/com/example/stoic/models/User.java
+++ Back/src/main/java/com/example/stoic/models/User.java
@@ -31,4 +31,65 @@ public class User {
     @Column(name="language")
     private Language language;
 
+
+    public User(String username, String email, String password, UserRole userRole, Language language) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.userRole = userRole;
+        this.language = language;
+    }
+    public User() {}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "idUser=" + idUser +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", userRole=" + userRole +
+                ", language=" + language +
+                '}';
+    }
 }
